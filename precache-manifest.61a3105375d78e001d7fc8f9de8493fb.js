8a5e31c9e9c3ffd8137ade2db23d89a03bcabb62
blackcat <suwei9797@gmail.com>
2019-04-24 23:07:22 +0800

    Updates

---
+++ precache-manifest.61a3105375d78e001d7fc8f9de8493fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2233d38602cbb0a33157",
+    "url": "/fedhub_beta/static/css/main.e6cfc81d.chunk.css"
+  },
+  {
+    "revision": "2233d38602cbb0a33157",
+    "url": "/fedhub_beta/static/js/main.cabe1a2c.chunk.js"
+  },
+  {
+    "revision": "1d9fd55b976dea637832",
+    "url": "/fedhub_beta/static/js/runtime~main.f31351da.js"
+  },
+  {
+    "revision": "bb3061d7735844361560",
+    "url": "/fedhub_beta/static/js/2.95d53e36.chunk.js"
+  },
+  {
+    "revision": "4ab9158e06aa8e320b82a6c4c1db5a87",
+    "url": "/fedhub_beta/index.html"
+  }
+];
