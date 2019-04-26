406b2dfb5b62ec592f5a4bbfb161ac62dba46674
blackcat <suwei9797@gmail.com>
2019-04-26 19:08:47 +0800

    Updates

---
+++ precache-manifest.d58904ded25dd2c7cefc1b51f4e728db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a6681d92880e6d37ee9",
+    "url": "/fedhub_beta/static/css/main.9fa1d2ca.chunk.css"
+  },
+  {
+    "revision": "5a6681d92880e6d37ee9",
+    "url": "/fedhub_beta/static/js/main.0b7bfdd4.chunk.js"
+  },
+  {
+    "revision": "1d9fd55b976dea637832",
+    "url": "/fedhub_beta/static/js/runtime~main.f31351da.js"
+  },
+  {
+    "revision": "3b055772637d4393e5d0",
+    "url": "/fedhub_beta/static/js/2.310a0e74.chunk.js"
+  },
+  {
+    "revision": "45ec2f0d6d83f4bbaaafcf730c200367",
+    "url": "/fedhub_beta/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fedhub_beta/precache-manifest.5ea633678b6abc9d8778cdcf05863954.js"
+  "/fedhub_beta/precache-manifest.d58904ded25dd2c7cefc1b51f4e728db.js"
 );
 
 workbox.clientsClaim();
