837828bc4f397858aac06583efb121253f3ffa6d
blackcat <suwei9797@gmail.com>
2019-05-14 16:49:38 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fedhub_beta/precache-manifest.3f2cc89ce33593cf3378c153ab8d2ef9.js"
+  "/fedhub_beta/precache-manifest.4e2951862457609761a9390b587517ce.js"
 );
 
 workbox.clientsClaim();
